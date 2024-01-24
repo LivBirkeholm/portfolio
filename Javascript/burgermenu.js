c9f918df2f91b96cdb29f6293c5872b1585c3ece
Liv <libi0001@stud.kea.dk>
2024-01-24 15:54:03 +0100

    scroll to top button

--- Javascript/burgermenu.js
+++ Javascript/burgermenu.js
@@ -7,3 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
     burgerMenu.classList.toggle("open");
   });
 });
+
+// Lyt efter scroll-begivenheder
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+  // Vis eller skjul knappen baseret på rullepositionen
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  // Rul til toppen
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
