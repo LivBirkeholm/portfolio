6b0d8d100a1601dfed95559fffcd25863dfc0efa
Liv <libi0001@stud.kea.dk>
2024-02-01 19:25:48 +0100

    atelier - final result tekst

--- Javascript/burgermenu.js
+++ Javascript/burgermenu.js
@@ -25,3 +25,35 @@ function scrollToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//Before and after pictures
+function showImage(imageNumber) {
+  var beforeImage = document.getElementById("beforeImage");
+  var afterImage = document.getElementById("afterImage");
+
+  // Skift stien til billederne baseret på dit behov
+  switch (imageNumber) {
+    case 1:
+      beforeImage.src = "img/before1.webp";
+      afterImage.src = "img/after1.webp";
+      break;
+    case 2:
+      beforeImage.src = "img/before2.webp";
+      afterImage.src = "img/after2.webp";
+      break;
+    case 3:
+      beforeImage.src = "img/before3.webp";
+      afterImage.src = "img/after3.webp";
+      break;
+    case 4:
+      beforeImage.src = "img/before4.webp";
+      afterImage.src = "img/after4.webp";
+      break;
+    case 5:
+      beforeImage.src = "img/before5.webp";
+      afterImage.src = "img/after5.webp";
+      break;
+    default:
+      break;
+  }
+}
